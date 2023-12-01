3c27008be458f9fbe3910eb74b20bfcb9c5e4ced
yuichiyasui <yassan.b.a.w@gmail.com>
2023-12-01 23:43:19 +0900

    Setup mdx

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+};
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
+});
+
+export default withMDX(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Hello World!</h1>
+      <Link href="/profile">Profile</Link>
     </main>
   );
 }
